3aa7eaaeea44617be4ae8eb3d5fe62e17d5acaea
Bettes2022 <104280447+Bettes2022@users.noreply.github.com>
2024-11-25 07:45:56 -0300

    correcao

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,18 @@
-criarCartao(
+criaCartao(
     'Exercícios para Hipertrofia Muscular',
     'Para gerar hipertofia muscular quantas séries e repetições são necessárias?',
     ' São necessárias de 3 a 4 séries com 12 repetições em cada set para se gerar um bom resultado de hipertrofia.'
 )
 
-criarCartao(
+criaCartao(
     'Exercícios para Hipertrofia Muscular',
     'Quais são os 4 exercícios básicos na musculação para desenvolver o corpo? ',
     'Os 4 exercícios básicos são: Agachamento livre, supino reto, levantamento terra e barra fixa.'
 )
 
-criarCartao(
+criaCartao(
     'Exercícios para Hipertrofia Muscular',
     'Cardio antes ou depoiS do treino de musculação?',
     ' Os estudos, nesse caso, se dividem. Alguns apontam que o cardio antes de treinar é aconselhável, enquanto outros indicam que o exercício aeróbico seja feito aṕos o treinamento. Isso irá depender do seu objetivo.'
 )
+
